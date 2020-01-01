5e5dc7f9f82c0d691d0b92a95f90b88a30e156f6
YuChih Hu <hijac.hu@gmail.com>
2020-01-01 18:43:58 +0800

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+}, {
+    id: 3,
+    description: 'Meeting with boss',
+    completed: true
+}];
 
 app.get('/', (req, res) => {
     res.send('Todo API Root');
+});
+
+// GET /todos/
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+
+
+    // Iterate of todos array. Find the match
+    todos.forEach(todo => {
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
 });
 
 app.listen(PORT, () => {
