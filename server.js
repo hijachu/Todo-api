6180c6c9c743aef8d6060584b09b09869128a180
YuChih Hu <hijac.hu@gmail.com>
2020-01-02 23:33:28 +0800

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Meeting with boss',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Todo API Root');
@@ -42,6 +35,19 @@ app.get('/todos/:id', (req, res) => {
     } else {
         res.status(404).send();
     }
+});
+
+app.post('/todos', (req, res) => {
+    var body = req.body;
+
+    body.id = todoNextId;
+    todoNextId++;
+    
+    todos.push(body);
+
+    // console.log('description: ' + body.description);
+
+    res.json(body);
 });
 
 app.listen(PORT, () => {
